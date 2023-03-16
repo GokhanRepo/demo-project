b2d7e55763b94beea022fa11b14a4c2fa7805209
GokhanRepo <113057065+GokhanRepo@users.noreply.github.com>
2023-03-16 18:41:32 +0300

    jenkins demo project

---
+++ src/test/java/cbt/CukesRunner.java
@@ -0,0 +1,17 @@
+package cbt;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "json:target/cucumber.json",
+                "html:target/cucumber/report.html",
+                "junit:target/junit/junit-report.xml",
+                "rerun:target/rerun.txt",},
+        features = "src/test/resources/cbt",
+        publish = true
+)
+public class CukesRunner {}
